ae9e2a92046917e11d1aacc1d477c902074a157d
Luis Lau <theauthorgm@gmail.com>
2018-10-23 19:47:19 -0500

    add auth settings to config file

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	PORT: process.env.PORT || 8080,
-	DB_URL: process.env.DB_URL || 'mongodb://localhost/grillbooking'
+	DB_URL: process.env.DB_URL || 'mongodb://localhost/grillbooking',
+	JWT_SECRET: process.env.JWT_SECRET || '123abc!@#',
+	JWT_EXPIRY: process.env.JWT_EXPIRY || '1d'
 };
