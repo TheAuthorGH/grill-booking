2e171afc487e3247fb44ca992e386a6fe01bc98d
Luis Lau <theauthorgm@gmail.com>
2018-10-23 19:43:51 -0500

    improve registration mechanism

--- client/scripts/page-register.js
+++ client/scripts/page-register.js
@@ -1,25 +1,31 @@
 function handleRegisterControls() {
+
 	$('.gb-register').submit(function(evt) {
 		evt.preventDefault();
+
+		const messageArea = $('.gb-register-message').css('color', 'inherit').text('Creating your account...');
+
+		const email = $('#gb-register-email').val().trim();
+		const password = $('#gb-register-password').val().trim();
+
 		$.ajax({
 			type: 'POST',
 			url: '/users',
 			dataType: 'json',
 			contentType: 'application/json',
 			data: JSON.stringify({
-				email: $('#gb-register-email').val(),
-				password: $('#gb-register-password').val()
+				email: email,
+				password: password
 			})
 		})
 		.done(() => {
 			window.location.href = "/login";
 		})
 		.fail(res => {
-			const messageArea = $('.gb-register-message').css('color', '#F33');
-			if(res.status === 400) // Not the best way to check for an invalid email!
-				messageArea.text('Email is already in use!');
+			if(res.status === 400)
+				messageArea.css('color', '#F33').text(res.responseText);
 			else
-				messageArea.text('Sorry, something went wrong and we could not create your account. Please try again later.');
+				messageArea.css('color', '#F33').text('Sorry, something went wrong and we could not create your account. Please try again later.');
 		});
 	});
 }
