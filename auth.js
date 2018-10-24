d593d14f548514acefb727261fde0a53b412d3eb
Luis Lau <theauthorgm@gmail.com>
2018-10-23 19:35:12 -0500

    implement auth mechanism

---
+++ auth.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Users = require('./persistence/model-users');
+const config = require('./config');
+
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { Strategy: LocalStrategy} = require('passport-local');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+
+function validatePassword(user, password) {
+	if(user)
+		return bcrypt.compare(password, user.password);
+	else
+		return new Promise(resolve => resolve(false));
+}
+
+function createAuthToken(user) {
+	return jwt.sign({user: {id: user.id}}, config.JWT_SECRET, {
+		subject: user.id.toString(),
+		expiresIn: config.JWT_EXPIRY,
+		algorithm: 'HS256'
+	});
+}
+
+passport.use(new LocalStrategy({usernameField: 'email'}, async (email, password, callback) => {
+	let user;
+	return Users.findOne({email: email})
+		.then(_user => {
+			user = _user;
+			if(user)
+				return validatePassword(user, password);
+			else
+				return Promise.reject({reason: 'login-notfound'});
+		})
+		.then(valid => {
+			if(valid)
+				return callback(null, user.serialize());
+			else
+				return Promise.reject({reason: 'login-invalid'});
+		})
+		.catch(err => {
+			if(err.reason === 'login-invalid' || err.reason === 'login-notfound')
+				return callback(null, false, err);
+			return callback(err, false);
+		});
+}));
+
+const localAuth = passport.authenticate('local', { session: false });
+
+passport.use(new JwtStrategy({
+	secretOrKey: config.JWT_SECRET,
+	jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+	algorithms: ['HS256']
+}, async (payload, done) => {
+	return Users.findById(payload.user.id)
+		.then(user => {
+			if(user)
+				done(null, user);
+			else
+				done(null, false);
+		})
+		.catch(err => done(err));
+}));
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failureRedirect: '/login' });
+
+module.exports = { validatePassword, createAuthToken, localAuth, jwtAuth };
